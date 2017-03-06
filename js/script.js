b34c058f95d9bce1862df5eaefe529b2e484c4dc
Niklas Schoenberger <nik.schoe@icloud.com>
2017-03-06 14:18:42 +0100

    stuff

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-/*$(document).on('click', '.scroll-btn', function(event){
+$(document).on('click', '.scroll-btn', function(event){
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top}, 850);
-});*/
+        scrollTop: $( $.attr(this, 'href') ).offset().top}, 1100);
+});
 
 $(document).ready (function() {
     var mainPoint = $("#main").offset().top;
     $(window).scroll(function() {
         if ($(window).scrollTop() >= mainPoint) {
-            $('.menubar').fadeIn();
+            $('.menubar').fadeIn(300).css({display: 'flex'})
         } else {
-            $('.menubar').fadeOut();
+            $('.menubar').fadeOut(300);
         }
     });
-})
+});
+
+
+// fadeIn back-to-top .scroll-btn
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y >= 1000) {
+        $('.up-btn').fadeIn();
+    } else {
+        $('.up-btn').fadeOut();
+    }
+});
+
